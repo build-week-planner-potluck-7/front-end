fce0af881b0607b02d924a43cbed4903037fb3e3
Brittany Petersen <britt.petersen100@gmail.com>
2021-12-22 15:24:57 -0700

    made some edits to foodform to be able to work with api

--- src/Components/foodForm.js
+++ src/Components/foodForm.js
@@ -1,17 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+
+const initialFood = []
 
 const FoodForm = () => {
 
     const [formValues, setFormValues] = useState([{ foodType: '', foodItem: ''}]);
+    const [food, setFood] = useState(initialFood)
 
-    const submit = () => {
-        const newFoodList = {
-            foodType: formValues.foodType,
-            foodItem: formValues.foodItem,
-        }
-        return newFoodList;
+
+
+    const getFood = () => {
+        axios.get('https://reqres.in/api/potluck')
+            .then(resp => {
+                setFood(resp.data.data)
+            }).catch(err => console.error(err))
+    }
+    
+    const postFood = newFood => {
+        axios.post('https://reqres.in/api/potluck', newFood)
+            .then(resp => {
+                setFood([ resp.data, ...food])
+            }).catch(err => console.error(err))
     }
 
     const handleSubmit = evt => {
@@ -35,6 +47,18 @@ const FoodForm = () => {
         setFormValues(newFormValues)
     }
 
+    const submit = () => {
+        const newFoodList = {
+            foodType: formValues.foodType,
+            foodItem: formValues.foodItem,
+        }
+        postFood(newFoodList);
+    }
+
+    useEffect(() => {
+        getFood()
+    }, [])
+
 
     return (
         <form className='form-page' onSubmit={handleSubmit}>
@@ -42,27 +66,6 @@ const FoodForm = () => {
             <h2>What food do you want at your potluck?</h2>
             </div>
             <div className='form'>
-                <div>
-                    <label>
-                        <select className='type-dropdown' value={formValues.foodType} onChange={handleChange}name='foodType'>
-                            <option value='' >--Select Type--</option>
-                            <option value='main-course'>Main Course</option>
-                            <option value='appetizer'>Appetizer</option>
-                            <option value='side-dish'>Side Dish</option>
-                            <option value='beverage'>Beverage</option>
-                            <option value='dessert'>Dessert</option>
-                        </select>
-                    </label>
-                    <label>
-                    <input 
-                        type='text'
-                        value={formValues.foodItem}
-                        name='foodItem'
-                        onChange={handleChange}
-                        placeholder='Hamburgers'
-                    />
-                    </label>
-                </div>
             {formValues.map((item, index) => (
                 <div className='form-input' key={index}>
                     <label>
@@ -95,7 +98,7 @@ const FoodForm = () => {
                 </div>
                 <div className='button-section'>
                     <button className="button add" type="button" onClick={() => addFormField()}>Add Item</button>
-                    <Link to='./guests' className='formLink'>Invite Guests</Link>
+                    <Link to='./guests' className='formLink' onClick={submit}>Invite Guests</Link>
                 </div>
 
         </form>
