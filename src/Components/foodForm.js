fcad8f5e6f2629fd887d9b8a68142c0be453c029
Jorge Evangelista <jorgeevangelista500@gmail.com>
2021-12-23 16:07:52 -0500

    fixed foodform

--- src/Components/foodForm.js
+++ src/Components/foodForm.js
@@ -1,110 +1,131 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-
-const initialFood = []
+const initialFood = [];
 
 const FoodForm = () => {
+  const [formValues, setFormValues] = useState([
+    { foodType: '', foodItem: '' },
+  ]);
+  console.log('formvalues', formValues);
+  const [food, setFood] = useState(initialFood);
 
-    const [formValues, setFormValues] = useState([{ foodType: '', foodItem: ''}]);
-    const [food, setFood] = useState(initialFood)
+  const getFood = () => {
+    axios
+      .get('https://lambda-build-week.herokuapp.com/foods/1')
+      .then((resp) => {
+        setFood(resp.data);
+      })
+      .catch((err) => console.error(err));
+  };
 
+  const postFood = (newFood) => {
+    axios
+      .post('https://lambda-build-week.herokuapp.com/foods', newFood)
+      .then((resp) => {
+        setFood([resp.data, ...food]);
+      })
+      .catch((err) => console.error(err));
+  };
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
 
-    const getFood = () => {
-        axios.get('https://lambda-build-week.herokuapp.com/foods/1')
-            .then(resp => {
-                setFood(resp.data)
-            }).catch(err => console.error(err))
-    }
-    
-    const postFood = newFood => {
-        axios.post('https://lambda-build-week.herokuapp.com/foods', newFood)
-            .then(resp => {
-                setFood([ resp.data, ...food])
-            }).catch(err => console.error(err))
-    }
+  const handleChange = (index, value, type) => {
+    setFormValues([
+      ...formValues.slice(0, index),
+      { ...formValues[index], [type]: value },
+      ...formValues.slice(index + 1),
+    ]);
+  };
 
-    const handleSubmit = evt => {
-        evt.preventDefault()
-        submit();
-    }
+  let addFormField = () => {
+    setFormValues([...formValues, { foodType: '', foodItem: '' }]);
+  };
 
-    let handleChange = (i, evt) => {
-        let newFormValues = [...formValues];
-        newFormValues[i][evt.target.name] = evt.target.value;
-        setFormValues(newFormValues);
-    }
+  let removeFormField = (item) => {
+    let newFormValues = [...formValues];
+    newFormValues.splice(item, 1);
+    setFormValues(newFormValues);
+  };
 
-    let addFormField = () => {
-        setFormValues([...formValues, { foodItem: ''}])
-    }
+  const submit = () => {
+    const newFoodList = {
+      foodType: formValues.foodType,
+      foodItem: formValues.foodItem,
+    };
+    postFood(newFoodList);
+  };
 
-    let removeFormField = (item) => {
-        let newFormValues = [...formValues];
-        newFormValues.splice(item, 1);
-        setFormValues(newFormValues)
-    }
+  useEffect(() => {
+    getFood();
+  }, []);
 
-    const submit = () => {
-        const newFoodList = {
-            foodType: formValues.foodType,
-            foodItem: formValues.foodItem,
-        }
-        postFood(newFoodList);
-    }
-
-    useEffect(() => {
-        getFood()
-    }, [])
-
-
-    return (
-        <form className='form-page' onSubmit={handleSubmit}>
-            <div>
-            <h2>What food do you want at your potluck?</h2>
-            </div>
-            <div className='form'>
-            {formValues.map((item, index) => (
-                <div className='form-input' key={index}>
-                    <label>
-                        <select className='type-dropdown' value={formValues.foodType} onChange={handleChange} name='foodType'>
-                            <option value=''>--Select Type--</option>
-                            <option value='main-course'>Main Course</option>
-                            <option value='appetizer'>Appetizer</option>
-                            <option value='side-dish'>Side Dish</option>
-                            <option value='beverage'>Beverage</option>
-                            <option value='dessert'>Dessert</option>
-                        </select>
-                    </label>
-                    <label>
-                    <input 
-                        type='text'
-                        value={formValues.foodItem}
-                        name='foodItem'
-                        onChange={handleChange}
-                        placeholder='Dish'
-                    />
-                    </label>
-                    {
-                        index ? 
-                        <button type="button"  className="button remove" onClick={() => removeFormField(index)}>Delete</button> 
-                        : null
-                    }
-
-                </div>
-                ))}
-                </div>
-                <div className='button-section'>
-                    <button className="button add" type="button" onClick={() => addFormField()}>Add Item</button>
-                    <Link to='./guests' className='formLink' onClick={submit}>Invite Guests</Link>
-                </div>
-
-        </form>
-    )
-
-
-}
+  return (
+    <form className='form-page' onSubmit={handleSubmit}>
+      <div>
+        <h2>What food do you want at your potluck?</h2>
+      </div>
+      <div className='form'>
+        {formValues.map((_, index) => (
+          <div className='form-input' key={index}>
+            <label>
+              <select
+                className='type-dropdown'
+                value={formValues.foodType}
+                onChange={(e) =>
+                  handleChange(index, e.target.value, e.target.name)
+                }
+                name='foodType'
+              >
+                <option value=''>--Select Type--</option>
+                <option value='main-course'>Main Course</option>
+                <option value='appetizer'>Appetizer</option>
+                <option value='side-dish'>Side Dish</option>
+                <option value='beverage'>Beverage</option>
+                <option value='dessert'>Dessert</option>
+              </select>
+            </label>
+            <label>
+              <input
+                type='text'
+                value={formValues.foodItem}
+                name='foodItem'
+                onChange={(e) =>
+                  handleChange(index, e.target.value, e.target.name)
+                }
+                placeholder='Dish'
+              />
+            </label>
+            {index ? (
+              <button
+                type='button'
+                className='button remove'
+                onClick={() => removeFormField(index)}
+              >
+                Delete
+              </button>
+            ) : null}
+          </div>
+        ))}
+      </div>
+      <div className='button-section'>
+        <button
+          className='button add'
+          type='button'
+          onClick={() => addFormField()}
+        >
+          Add Item
+        </button>
+        <Link to='./guests' className='formLink' onClick={submit}>
+          Invite Guests
+        </Link>
+      </div>
+    </form>
+  );
+};
 
 export default FoodForm;
